07e1c674188977055d070d74f592b15c28b06aea
= <=>
2022-08-22 19:56:15 +0530

    node js start

---
+++ NodeJs/test.js
@@ -0,0 +1,8 @@
+console.log("running in node js")
+let a=3
+let b = 5
+console.log(a+b)
+const os = require('node:os');
+console.log(os.freemem())
+console.log(os.totalmem())
+console.log(os.platform())
